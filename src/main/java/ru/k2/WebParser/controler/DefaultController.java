829be18c34d0e8a7deb03f8869cc901152793d24
Kirill Smirnov <loha8088@gmail.com>
2020-12-18 21:58:20 +0300

    Добавлен контролер и html страница

---
+++ src/main/java/ru/k2/WebParser/controler/DefaultController.java
@@ -0,0 +1,13 @@
+package ru.k2.WebParser.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String starter(){
+        return "starter";
+    }
+}
