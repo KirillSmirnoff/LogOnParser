dbf5ad4452ab895fa4e1ff218b1f82a94cec2cdf
Kirill Smirnov <loha8088@gmail.com>
2020-12-22 00:37:31 +0300

    DTO для результата

---
+++ src/main/java/ru/k2/WebParser/domain/ResultData.java
@@ -0,0 +1,13 @@
+package ru.k2.WebParser.domain;
+
+public class ResultData {
+    String result ;
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
