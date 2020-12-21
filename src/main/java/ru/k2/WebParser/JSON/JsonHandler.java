1a247fd2891f99f015f889026994cc5fb5864c5f
Kirill Smirnov <loha8088@gmail.com>
2020-12-22 01:03:04 +0300

    Удалил мусор

--- src/main/java/ru/k2/WebParser/JSON/JsonHandler.java
+++ src/main/java/ru/k2/WebParser/JSON/JsonHandler.java
@@ -70,65 +70,10 @@ public class JsonHandler {
         saveJsonToFile(sb.toString()); // сохраняем новый ru.k2.WebParser.JSON файл на комп
     }
 
-    /*метод используется, если данные поступают со станддартного ввод-вывод*/
-//    public void jsonHandlerFromStandardIO() throws IOException {
-//        ReadDataFromStandardIO readFromIO = new ReadDataFromStandardIO();
-//        DataBase dataBase = new DataBase(); /*если значение тегов берется из БД*/
-//        DataFromProperties dbProp = new DataFromProperties(); /*если значение тегов береться из файла*/
-//        Properties properties = dbProp.loadProperties();
-//
-//        /*<БЛОК ОТВЕЧАЮШИЙ ЗА ПРЕОБРАЗОВАНИЕ В ru.k2.WebParser.JSON>*/
-//        String xmlString = readFromIO.inToString();    /*преобразуем данные со стандартного ввода в строку - String*/
-//        String jsonString = xmlToJsonString(xmlString);  /*преобразуем переданные данные строка-ru.k2.WebParser.XML  в ru.k2.WebParser.JSON*/
-//        StringBuilder sb = new StringBuilder(jsonString);
-//        String tagName = null;
-//
-//        /*<БЛОК ОТВЕЧАЮШИЙ ЗА ПАРСИНГ ru.k2.WebParser.JSON >*/
-//        JsonFactory jsonFactory = new JsonFactory();
-//        JsonParser parser = jsonFactory.createParser(jsonString);
-//
-//        /*находит в ru.k2.WebParser.JSON заголовок FIELD_NAME
-//        * получает значени заголовка (тег-xml), и вычисляет начало и конец (расположение) в ru.k2.WebParser.JSON файле
-//        * далее данное расположение (начало и конец) потребуются для замены тег-xml на значение данного из БД*/
-//        while (!parser.isClosed()) {
-//            JsonToken jsonToken = parser.nextToken();
-//            if (jsonToken != null && jsonToken.equals(FIELD_NAME)) {
-//                tagName = parser.getText();
-//                int startIndex = sb.indexOf(tagName); /*получем начальное расположение тега-xml в ru.k2.WebParser.JSON файле*/
-//                int finalIndex = 0;
-//                    if (parser.getText().length()==1) { /*получаем конечное расположение тега-xml согласно условию*/
-//                        finalIndex = startIndex+1;
-//                     }
-//                    else
-//                        finalIndex = startIndex + parser.getText().length();
-//
-//                /* БЛОК ОТВЕЧАЮШИЙ ЗА ПОИСК ТЕГА В БД ИЛИ БАЗЕ-ЗНАНИЙ(ФАЙЛ) */
-//
-//                /* метод считывает значение тега-xml из БД */
-//             //sb.replace(startIndex,finalIndex, dataBase.getFromDB(parser.getText()));
-//
-//                /*метод считывает значения тега-xml из БазыЗнаний(файл)*/
-//                sb.replace(startIndex,finalIndex, properties.getProperty(tagName));
-//
-//                /*тестовая реализация для проверки ru.k2.WebParser.JSON заголовков
-////            if (jsonToken != null && (jsonToken.equals(VALUE_STRING) || jsonToken.equals(VALUE_NUMBER_INT) || jsonToken.equals(VALUE_NUMBER_FLOAT))) {
-////            if (jsonToken != null && jsonToken.equals(FIELD_NAME)) {
-////                System.out.println(dataBase.getFromDB(parser.getText()));
-//*/
-//            }
-//        }
-//        /* БЛОК ОТВЕЧАЮШИЙ ЗА ЗАПИСЬ РЕЗУЛЬТАТА В ФАЙЛ*/
-//
-//        /*   saveJsonToFile(sb.toString());  сохраняем новый ru.k2.WebParser.JSON файл на комп
-//         * далее открываем данный файл */
-////        LocalFileAction.getDesktop().open(saveJsonToFile(sb.toString()));
-//    }
-
     public String webConverter(String xmlString) {
 
-
         Properties properties = new DataFromProperties().loadProperties();
-        String tagName = null;
+        String tagName ;
         String jsonString = "";
 
         try {
@@ -151,8 +96,8 @@ public class JsonHandler {
             e.printStackTrace();
         }
 
-        /*находит в ru.k2.WebParser.JSON заголовок FIELD_NAME
-         * получает значени заголовка (тег-xml), и вычисляет начало и конец (расположение) в ru.k2.WebParser.JSON файле
+        /*находит в заголовоке FIELD_NAME
+         * получает значени заголовка (тег-xml), и вычисляет начало и конец (расположение) в JSON файле
          * далее данное расположение (начало и конец) потребуются для замены тег-xml на значение данного из БД*/
         while (!parser.isClosed()) {
             JsonToken jsonToken = null;
@@ -160,8 +105,7 @@ public class JsonHandler {
                 jsonToken = parser.nextToken();
                 if (jsonToken != null && jsonToken.equals(FIELD_NAME)) {
                     tagName = parser.getText();
-                    startIndex = sb.indexOf(tagName); /*получем начальное расположение тега-xml в ru.k2.WebParser.JSON файле*/
-                    finalIndex = 0;
+                    startIndex = sb.indexOf(tagName); /*получем начальное расположение тега-xml в JSON файле*/
                     if (parser.getText().length() == 1) { /*получаем конечное расположение тега-xml согласно условию*/
                         finalIndex = startIndex + 1;
                     } else
--- src/main/java/ru/k2/WebParser/Launch.java
+++ src/main/java/ru/k2/WebParser/Launch.java
@@ -8,33 +8,7 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class Launch {
-
-//    public static final String UriString = "/home/k2/Desktop/Onelya/docs/P62G62_test_answerExample.xml";
-
     public static void main(String[] args) throws IOException {
-
         SpringApplication.run(Launch.class, args);
-//        File file = new File(UriString); //вркменно закаментил путь у xml файл
-//        JsonHandler jsonHandler = new JsonHandler();
-
-        /*метод используется, если данный поступают на станддартный ввод-вывод*/
-//        jsonHandler.jsonHandlerFromStandardIO();
-
-/*метод используется, если данный считываются с файла
-        jsonHandler.jsonHandlerFromFile(file);*/
-
     }
-
-    /*Парсинг ru.k2.WebParser.XML при помоши SAX ---- мметод не используется в текущей реализации
-    private static void xmlParser() {
-        XmlHandler xmlHandler = new XmlHandler();
-        try {
-            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            saxParser.parse(UriString, xmlHandler);
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-    */
 }
